7bde946aa5df076a8d8e4d34d5602caaef25b6d2
Tim <timwagner997@gmail.com>
2023-02-08 22:31:58 +0100

    improved layout of the randomizer page

--- lib/randomizer/randomizer_page.dart
+++ lib/randomizer/randomizer_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rando_meme/randomizer/cubit/randomizer_cubit.dart';
+import 'package:rando_meme/randomizer/model/meme.dart';
 
 class RandomizerPage extends StatelessWidget {
   const RandomizerPage({super.key});
@@ -19,30 +20,46 @@ class RandomizerPage extends StatelessWidget {
         title: const Text("RandoMeme 🎲"),
       ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Center(
-            child: BlocBuilder<RandomizerCubit, RandomizerState>(
-              builder: (context, state) {
-                if (state is RandomizerInitial) {
-                  return const Text(
-                    "Press the Button at the bottom right to fetch a new Meme. 🚀",
-                    textAlign: TextAlign.center,
-                  );
-                }
+        child: BlocBuilder<RandomizerCubit, RandomizerState>(
+          builder: (context, state) {
+            if (state is RandomizerInitial) {
+              return const Text(
+                "Press the Button at the bottom right to fetch a new Meme. 🚀",
+                textAlign: TextAlign.center,
+              );
+            }
 
-                if (state is RandomizerMemeReceived) {
-                  return Image.network(state.meme.url);
-                }
+            if (state is RandomizerMemeReceived) {
+              final Meme meme = state.meme;
 
-                if (state is RandomizerFetchFailed) {
-                  return Text(state.reason);
-                }
+              return Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(flex: 2, child: Image.network(meme.url)),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.public),
+                        ),
+                        Text("Author: ${meme.author}"),
+                      ],
+                    ),
+                    const Spacer(),
+                  ],
+                ),
+              );
+            }
 
-                return const CircularProgressIndicator();
-              },
-            ),
-          ),
+            if (state is RandomizerFetchFailed) {
+              return Text(state.reason);
+            }
+
+            return const Center(child: CircularProgressIndicator());
+          },
         ),
       ),
     );
