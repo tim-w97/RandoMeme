a2be257ed67261822c54a2c72d2718f354812bf3
Tim <timwagner997@gmail.com>
2023-02-08 21:47:18 +0100

    added a model class for the fetched memes

---
+++ lib/randomizer/model/meme.dart
@@ -0,0 +1,31 @@
+class Meme {
+  final String postLink;
+  final String url;
+  final String author;
+  final int ups;
+
+  Meme({
+    required this.postLink,
+    required this.url,
+    required this.author,
+    required this.ups,
+  });
+
+  Meme fromJson(Map<String, dynamic> json) {
+    return Meme(
+      postLink: json["postLink"],
+      url: json["url"],
+      author: json["author"],
+      ups: json["ups"],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "postLink": postLink,
+      "url": url,
+      "author": author,
+      "ups": ups,
+    };
+  }
+}
