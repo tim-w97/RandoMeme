db404b8e9d2fcaa78d1a7a2aae82110533ffe9e5
Tim <timwagner997@gmail.com>
2023-02-08 20:35:45 +0100

    added a repository to fetch random memes

--- lib/randomizer/cubit/randomizer_cubit.dart
+++ lib/randomizer/cubit/randomizer_cubit.dart
@@ -1,8 +1,11 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:rando_meme/randomizer/meme_repository.dart';
 
 part 'randomizer_state.dart';
 
 class RandomizerCubit extends Cubit<RandomizerState> {
-  RandomizerCubit() : super(RandomizerInitial());
+  MemeRepository memeRepository;
+
+  RandomizerCubit({required this.memeRepository}) : super(RandomizerInitial());
 }
--- lib/randomizer/meme_repository.dart
+++ lib/randomizer/meme_repository.dart
@@ -1,1 +1,18 @@
-class MemeRepository {}
+import 'dart:io';
+
+import 'package:http/http.dart' as fetcher;
+
+class MemeRepository {
+  Future<String> fetchRandomMeme() async {
+    final uri = Uri.https("https://meme-api.com/gimme");
+
+    final response = await fetcher.get(uri);
+
+    if (response.statusCode != HttpStatus.ok) {
+      throw Exception(
+          "Failed to fetch the meme image. Reason: ${response.reasonPhrase}");
+    }
+
+    return response.body;
+  }
+}
